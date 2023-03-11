42ab3ac42929f13d035b775dbdabad3b95cbf363
Atomis14 <michelsabbatini@outlook.com>
2023-03-11 23:47:19 +0100

    implement auto logout when server returns 401

--- components/Notification.vue
+++ components/Notification.vue
@@ -1,12 +1,11 @@
 <template>
-  <div
-    :key="notificationStore.notification?.id"
-    :class="`bg-${color}-100 border-${color}-300 text-${color}-800`"
-    class="notification border border-solid fixed left-1/2 -translate-x-1/2 py-2 px-4 top-3 rounded shadow-lg pointer-events-none max-lg"
-    v-if="notificationStore.notification"
-  >
-    {{ notificationStore.notification?.content }}
-  </div>
+  <Transition>
+    <div :key="notificationStore.notification?.id" :class="`bg-${color}-100 border-${color}-300 text-${color}-800`"
+      class="notification border border-solid fixed left-1/2 -translate-x-1/2 py-2 px-4 top-3 rounded shadow-lg pointer-events-none max-lg"
+      v-if="notificationStore.notification">
+      {{ notificationStore.notification?.content }}
+    </div>
+  </Transition>
 
   <!-- Div with all , because tailwind only includes used -->
   <div class="opacity-0 bg-red-100 border-red-300 text-red-800"></div>
@@ -15,53 +14,46 @@
 </template>
 
 <script lang="ts" setup>
-  // Possible Types: alert, warning, success; if another string is set, it defaults to success
+// Possible Types: alert, warning, success; if another string is set, it defaults to success
 
-  import { useNotificationStore } from '~~/stores/notification.store';
+import { useNotificationStore } from '~~/stores/notification.store';
 
-  const notificationStore = useNotificationStore();
+const notificationStore = useNotificationStore();
 
-  const color = ref('');
+const color = ref('');
 
-  const updateNotificationColor = (state: any) => {
-    switch (state.notification?.type) {
-      case 'alert':
-        color.value = 'red';
-        break;
-      case 'warning':
-        color.value = 'amber';
-        break;
-      default:
-      case 'success':
-        color.value = 'emerald';
-        break;
-    }
+const updateNotificationColor = (state: any) => {
+  switch (state.notification?.type) {
+    case 'alert':
+      color.value = 'red';
+      break;
+    case 'warning':
+      color.value = 'amber';
+      break;
+    default:
+    case 'success':
+      color.value = 'emerald';
+      break;
   }
+}
 
-  // set notification color the first time the component is mounted
-  updateNotificationColor(notificationStore);
-  // subscribe to changes in the store such that the colors can always be adjusted
-  notificationStore.$subscribe((mutations, state) => {
-    updateNotificationColor(state);
-  })
+// set notification color the first time the component is mounted
+updateNotificationColor(notificationStore);
+// subscribe to changes in the store such that the colors can always be adjusted
+notificationStore.$subscribe((mutations, state) => {
+  updateNotificationColor(state);
+})
 </script>
 
 <style scoped>
-  @keyframes anim {
-    from {
-      margin-top: 0;
-      opacity: 1;
-    }
-    to {
-      margin-top: -20px;
-      opacity: 0;
-    }
-  }
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.3s ease;
+}
 
-  .notification {
-    animation-name: anim;
-    animation-duration: 0.5s;
-    animation-delay: 3s;
-    animation-fill-mode: forwards;  /* keep final state */
-  }
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+  margin-top: -20px;
+}
 </style>
--- composables/useApi.ts
+++ composables/useApi.ts
@@ -1,13 +1,29 @@
 import axios from 'axios';
+import { useAuthStore } from '~~/stores/auth.store';
 
 export const useApi = () => {
   const config = useRuntimeConfig();
-  const api =  axios.create({
-    baseURL: config.public.baseURL
+  const authStore = useAuthStore();
+  const api = axios.create({
+    baseURL: config.public.baseURL,
   });
-  api.interceptors.request.use(config => {
+  api.interceptors.request.use((config) => {
+    // append auth token to every request
     config.headers.Authorization = 'Bearer ' + localStorage.getItem('token');
     return config;
   });
+  api.interceptors.response.use(
+    (response) => {
+      // 2xx errors are handeled normally
+      return response;
+    },
+    (error) => {
+      // logout if server returns 401 error --> JWT probably expired, need to login again
+      if (error.response.status === 401) {
+        authStore.logout();
+      }
+      return Promise.reject(error);
+    }
+  );
   return api;
-}
+};
