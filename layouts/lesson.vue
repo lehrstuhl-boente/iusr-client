baf78eb112280d6f1132af373d7145d1417938b7
Atomis14 <michelsabbatini@outlook.com>
2023-03-30 14:38:48 +0200

    lesson skeleton

--- layouts/lesson.vue
+++ layouts/lesson.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-    <header class="flex items-center text-white bg-slate-900 sticky top-0 z-10 p-4">
-      <NuxtLink :to="'/admin/course/' + $route.params.courseId"
-        class="flex justify-center hover:text-white hover:opacity-70 active:opacity-60">
-        Quit
-      </NuxtLink>
-    </header>
     <slot />
   </div>
 </template>
--- pages/admin/course/[courseId]/lesson/[lessonId].vue
+++ pages/admin/course/[courseId]/lesson/[lessonId].vue
@@ -1,5 +1,20 @@
 <template>
-  LESSON
+  <div class="h-screen flex flex-col">
+    <header class="flex items-center text-white bg-gray-900 sticky top-0 z-10 p-4">
+      <NuxtLink :to="'/admin/course/' + $route.params.courseId"
+        class="flex justify-center hover:text-white hover:opacity-70 active:opacity-50">
+        Quit
+      </NuxtLink>
+    </header>
+    <div class="columns-3 gap-0 h-full">
+      <div class="h-full">TipTap Editor</div>
+      <div class="p-2 bg-gray-800 h-full">Monaco Editor</div>
+      <div class="p-2 bg-black h-full text-white">Output</div>
+    </div>
+    <div class="mt-auto bg-gray-900 w-full p-4 text-white">
+      Footer
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
