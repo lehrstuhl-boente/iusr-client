e867dc1bbbcc032a4502fbe66a2b1b6edbdaa040
Atomis14 <michelsabbatini@outlook.com>
2023-05-25 09:35:32 +0200

    combine admin and user lesson page into one

--- components/sidebar/SidebarLesson.vue
+++ components/sidebar/SidebarLesson.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="lesson">
-    <NuxtLink :to="`${authStore.isAdmin ? '/admin' : ''}/course/${$route.params.courseId}/lesson/${lesson.id}`"
+    <NuxtLink :to="`/course/${$route.params.courseId}/lesson/${lesson.id}`"
       class="block py-2 px-3 rounded-lg text-black hover:text-primary hover:bg-indigo-50 active:bg-indigo-100 active:text-primary-active"
       :class="{ active: lesson.id == $route.params.lessonId }">
       {{ lesson.title }}
--- pages/course/[courseId]/lesson/[lessonId].vue
+++ pages/course/[courseId]/lesson/[lessonId].vue
@@ -3,27 +3,36 @@
     <Sidebar :show="showSidebar" @close="showSidebar = false" />
   </Teleport>
   <div class="h-screen flex flex-col" v-if="course && lesson">
-    <header class="flex items-center text-white bg-dark z-10 p-2" style="height: 60px;">
-      <button class="material-icons-outlined mr-1 icon-btn icon-btn-light" @click="showSidebar = true">menu</button>
-      <div class="ml-2">
-        <div class="opacity-50 text-xs">{{ course.title }}</div>
-        <div class="text-sm">{{ lesson.title }}</div>
+    <header class="flex items-center text-white bg-dark z-10 px-2" style="height: 60px;">
+      <div class="w-1/3 flex items-center">
+        <button class="material-icons-outlined mr-1 icon-btn icon-btn-light" @click="showSidebar = true">menu</button>
+        <div class="ml-2">
+          <div class="opacity-50 text-xs">Chapter {{ lesson.chapter.position }}: {{ lesson.chapter.title }}</div>
+          <div class="text-sm">Lesson {{ lesson.position }}: {{ lesson.title }}</div>
+        </div>
       </div>
-      <button class="inline-flex ml-auto py-2 px-3 rounded hover:bg-white hover:bg-opacity-10 active:bg-opacity-5"
-        @click="saveLesson">
-        <span class="material-icons-outlined mr-1">save</span>Save
-      </button>
+      <div class="w-1/3 text-center">
+        {{ course.title }}
+      </div>
+      <div class="w-1/3 text-right">
+        <button class="inline-flex ml-auto py-2 px-3 rounded hover:bg-white hover:bg-opacity-10 active:bg-opacity-5"
+          @click="saveLesson" v-if="authStore.isAdmin">
+          <span class="material-icons-outlined mr-1">save</span>Save
+        </button>
+      </div>
     </header>
     <div class="flex h-[calc(100%_-_120px)]">
       <div class="w-1/3 flex flex-col">
-        <RichtextEditor v-model="lesson.task" :readonly="true" />
+        <RichtextEditor v-model="lesson.task" :readonly="!authStore.isAdmin" />
       </div>
       <div class="w-1/3 h-full flex flex-col">
-        <CodeEditor v-model="lesson.userData.code" class="h-full" />
+        <CodeEditor v-model="lesson.code" class="h-full" v-if="authStore.isAdmin" />
+        <CodeEditor v-model="lesson.userData.code" class="h-full" v-if="!authStore.isAdmin" />
         <div class="p-2 mt-auto bg-dark flex items-center justify-end">
           <!-- <span class="material-icons-outlined text-white icon-btn icon-btn-light mr-auto">open_in_full</span> -->
-          <span class="material-icons-outlined ml-2 text-white icon-btn icon-btn-light"
-            @click="resetCode">restart_alt</span>
+          <LanguageSelect class="ml-1" v-if="authStore.isAdmin" />
+          <span class="material-icons-outlined ml-2 text-white icon-btn icon-btn-light" @click="resetCode"
+            v-if="!authStore.isAdmin">restart_alt</span>
           <button class="btn btn-primary ml-3">Run</button>
         </div>
       </div>
@@ -36,8 +45,8 @@
         class="inline-flex mr-auto py-2  px-3 rounded hover:bg-white hover:text-white hover:bg-opacity-10 active:bg-opacity-5">
         <span class="material-icons-outlined mr-1">close</span>Close
       </button>
-      <button class="btn">Previous</button>
-      <button class="btn ml-2">Next</button>
+      <button class="btn" @click="gotoPrevious" :disabled="lesson.previous === null">Previous</button>
+      <button class="btn ml-2" @click="gotoNext" :disabled="lesson.next === null">Next</button>
     </div>
   </div>
   <div v-else>Could not load course or lesson.</div>
@@ -52,6 +61,7 @@ definePageMeta({
 const route = useRoute();
 const courseStore = useCourseStore();
 const lessonStore = useLessonStore();
+const authStore = useAuthStore();
 
 const { course } = storeToRefs(courseStore);
 const { lesson } = storeToRefs(lessonStore);
@@ -63,12 +73,24 @@ await lessonStore.getLesson(parseInt(route.params.lessonId as string));
 
 const closeLesson = () => {
   if (confirm('Do you want to close this lesson? All unsaved changes will be lost.')) {
-    navigateTo('/course/' + route.params.courseId);
+    navigateTo(`${authStore.isAdmin ? '/admin' : ''}/course/${route.params.courseId}`);
   }
 }
 
 const saveLesson = async () => {
   await lessonStore.save();
+}
+
+const gotoPrevious = () => {
+  if (lesson.value && lesson.value.previous !== null) {
+    navigateTo(`/course/${route.params.courseId}/lesson/${lesson.value.previous}`);
+  }
+}
+
+const gotoNext = () => {
+  if (lesson.value && lesson.value.next !== null) {
+    navigateTo(`/course/${route.params.courseId}/lesson/${lesson.value.next}`);
+  }
 }
 
 const resetCode = () => {
