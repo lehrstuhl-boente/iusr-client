1f0f81abcdc89dc4f9454c4f547a428ac7a6afca
Atomis14 <michelsabbatini@outlook.com>
2023-07-05 17:00:11 +0200

    show progress bar for courses in dashboard

--- components/CourseCard.vue
+++ components/CourseCard.vue
@@ -1,9 +1,18 @@
 <template>
   <NuxtLink :to="`/course/${course.id}`"
     class="bg-white shadow hover:shadow-md rounded-md overflow-hidden hover:cursor-pointer">
-    <div class="bg-gray-200" style="height: 100px;"></div>
+    <div class="px-3 py-1 bg-gray-200 flex justify-between items-center">
+      <span class="font-bold">{{ progress }}%</span>
+      <span class="text-xs">{{ course.lessonsCompleted }}/{{ course.lessonsTotal }} Lessons Completed</span>
+    </div>
+    <div>
+      <div class="bg-primary" style="height: 2px;" :style="{ width: progress + '%' }"></div>
+    </div>
     <div class="content p-3">
       <strong>{{ course.title }}</strong>
+      <div class="muted mt-1 text-ellipsis leading-5 overflow-hidden whitespace-nowrap">
+        {{ course.description }}
+      </div>
     </div>
   </NuxtLink>
 </template>
@@ -12,4 +21,6 @@
 import { CourseDto } from '~~/types';
 
 const { course } = defineProps<{ course: CourseDto }>();
+
+const progress = Math.round((course.lessonsCompleted / course.lessonsTotal) * 100);
 </script>
--- types/courseDto.ts
+++ types/courseDto.ts
@@ -2,4 +2,6 @@ export interface CourseDto {
   id: number;
   title: string;
   description: string;
+  lessonsTotal: number;
+  lessonsCompleted: number;
 }
