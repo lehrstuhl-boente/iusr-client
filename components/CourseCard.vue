3f681d57ba9b1da11ea730ff9be9bca5df474891
Atomis14 <michelsabbatini@outlook.com>
2023-07-07 12:43:10 +0200

    fix line clamp bug

--- components/CourseCard.vue
+++ components/CourseCard.vue
@@ -3,15 +3,16 @@
     class="bg-white shadow hover:shadow-md active:shadow-sm rounded-md overflow-hidden hover:cursor-pointer">
     <div class="px-3 py-1 bg-primary/10 flex justify-between items-center">
       <span class="font-bold text-primary">{{ progress }}%</span>
-      <span class="text-xs text-primary/50">{{ course.lessonsCompleted }}/{{ course.lessonsTotal }} Lessons
-        Completed</span>
+      <span class="text-xs text-primary/50">
+        {{ course.lessonsCompleted }}/{{ course.lessonsTotal }} Lessons Completed
+      </span>
     </div>
     <div>
       <div class="bg-primary" style="height: 2px;" :style="{ width: progress + '%' }"></div>
     </div>
     <div class="content p-3">
       <strong>{{ course.title }}</strong>
-      <div class="muted leading-5 line-clamp-2 text-sm">{{ course.description }}</div>
+      <div class="muted leading-5 text-sm line-clamp-2">{{ course.description }}</div>
     </div>
   </NuxtLink>
 </template>
@@ -23,3 +24,13 @@ const { course } = defineProps<{ course: CourseDto }>();
 
 const progress = Math.round((course.lessonsCompleted / course.lessonsTotal) * 100);
 </script>
+
+<style scoped>
+/*property doesn't currently work with tailwind out of the box --> remove this when it works*/
+.line-clamp-2 {
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+}
+</style>
